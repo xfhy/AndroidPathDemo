d4682971ee33d90e592c68128bcff6504080217a
xfhy666 <xfhy666@gmail.com>
2019-03-28 09:09:58 +0800

    DownloadView  已经完成

--- DownloadView/app/src/main/java/com/xfhy/downloadview/DownloadView.java
+++ DownloadView/app/src/main/java/com/xfhy/downloadview/DownloadView.java
@@ -4,8 +4,8 @@ import android.animation.Animator;
 import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
@@ -13,11 +13,20 @@ import android.view.animation.LinearInterpolator;
 
 /**
  * Created by feiyang on 2019/3/27 18:29
- * Description :
+ * Description : 下载中View
+ * 画2根线,底部一根,上层一根
  */
 public class DownloadView extends View implements Animator.AnimatorListener {
 
     private static final String TAG = "DownloadView";
+    /**
+     * 刻度  1000  刻度越小,越平滑,障眼法
+     */
+    private static final int SCALE = 1000;
+    /**
+     * 默认动画时长
+     */
+    private static final int DEFAULT_DURATION = 10 * 1000;
     /**
      * 底层画笔
      */
@@ -27,16 +36,21 @@ public class DownloadView extends View implements Animator.AnimatorListener {
      */
     private Paint mTopPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
     private int mTopColor = 0xFF119CED;
-    private int mBottomColor = 0xFF999999;
+    private int mBottomColor = 0xFFE0E0E0;
     private int mWidth, mHeight, mRadius;
     private int mProgress = 0;
-    private int mDelay = 100;
     private LinearInterpolator mLinearInterpolator = new LinearInterpolator();
     /**
      * 中间可以滑动的区域
      */
     private double mTopEffectiveSlidingWidth;
     private ValueAnimator mLeftToRightAnim;
+    private boolean isAnimatorPause;
+    private AnimProgressListener mAnimProgressListener;
+    /**
+     * 动画时长
+     */
+    private int mDuration = DEFAULT_DURATION;
 
     public DownloadView(Context context) {
         this(context, null);
@@ -60,19 +74,17 @@ public class DownloadView extends View implements Animator.AnimatorListener {
         mBottomPaint.setStyle(Paint.Style.FILL);
         mBottomPaint.setStrokeCap(Paint.Cap.ROUND);
 
-        mLeftToRightAnim = ValueAnimator.ofFloat(0f, 500f, 1000f);
+        mLeftToRightAnim = ValueAnimator.ofFloat(0f, SCALE);
         mLeftToRightAnim.setInterpolator(mLinearInterpolator);
-        mLeftToRightAnim.setDuration(10 * 1000);
+        mLeftToRightAnim.setDuration(mDuration);
         mLeftToRightAnim.addUpdateListener(mLeftToRightAnimListener);
         mLeftToRightAnim.addListener(this);
-        mLeftToRightAnim.start();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        canvas.drawColor(Color.BLACK);
         canvas.drawLine(mRadius, mRadius, mWidth - mRadius, mRadius, mBottomPaint);
 
         double slideWidth = mTopEffectiveSlidingWidth * mProgress + mHeight;
@@ -82,7 +94,7 @@ public class DownloadView extends View implements Animator.AnimatorListener {
         } else {
             canvas.drawLine(mRadius, mRadius, (float) slideWidth, mRadius, mTopPaint);
         }
-       // Log.w(TAG, "onDraw:  slideWidth=" + slideWidth + "     mWidth=" + mWidth + "    mProgress=" + mProgress);
+        // Log.w(TAG, "onDraw:  slideWidth=" + slideWidth + "     mWidth=" + mWidth + "    mProgress=" + mProgress);
     }
 
     @Override
@@ -98,7 +110,7 @@ public class DownloadView extends View implements Animator.AnimatorListener {
         mTopPaint.setStrokeWidth(mHeight);
         mBottomPaint.setStrokeWidth(mHeight);
         mRadius = mHeight / 2;
-        mTopEffectiveSlidingWidth = (mWidth - mHeight) * 1.0 / 1000;
+        mTopEffectiveSlidingWidth = (mWidth - mHeight) * 1.0 / SCALE;
     }
 
     private ValueAnimator.AnimatorUpdateListener mLeftToRightAnimListener = new ValueAnimator.AnimatorUpdateListener() {
@@ -110,10 +122,71 @@ public class DownloadView extends View implements Animator.AnimatorListener {
             if (mProgress % 10 == 0) {
                 //回调  进度  x%
                 Log.w(TAG, "progress: " + mProgress / 10);
+                if (mAnimProgressListener != null) {
+                    mAnimProgressListener.progressUpdate(mProgress / 10);
+                }
             }
             invalidate();
         }
     };
+
+    /**
+     * 设置动画时长
+     */
+    public void setDuration(int duration) {
+        mDuration = duration;
+        mLeftToRightAnim.setDuration(mDuration);
+    }
+
+    public void setTopColor(int topColor) {
+        mTopColor = topColor;
+    }
+
+    public void setBottomColor(int bottomColor) {
+        mBottomColor = bottomColor;
+    }
+
+    /**
+     * 开始动画
+     */
+    public void start() {
+        mLeftToRightAnim.start();
+    }
+
+    /**
+     * 动画暂停
+     */
+    public void pause() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            mLeftToRightAnim.pause();
+        } else {
+            if (!isAnimatorPause) {
+                mLeftToRightAnim.cancel();
+                isAnimatorPause = true;
+            }
+        }
+    }
+
+    /**
+     * 动画继续
+     */
+    public void continueAnim() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            mLeftToRightAnim.resume();
+        } else {
+            //为minSDK 19以下做兼容
+            mLeftToRightAnim.setFloatValues(mProgress, SCALE);
+            mLeftToRightAnim.setDuration((mDuration / 1000) * (SCALE - mProgress));
+            mLeftToRightAnim.start();
+        }
+    }
+
+    /**
+     * 直接到达终点 100%
+     */
+    public void stop() {
+        mLeftToRightAnim.end();
+    }
 
     @Override
     public void onAnimationStart(Animator animation) {
@@ -123,15 +196,35 @@ public class DownloadView extends View implements Animator.AnimatorListener {
     @Override
     public void onAnimationEnd(Animator animation) {
         //动画结束 了  记得回调
+        Log.w(TAG, "onAnimationEnd: 动画结束");
     }
 
     @Override
     public void onAnimationCancel(Animator animation) {
-
+        Log.w(TAG, "onAnimationCancel: 动画取消");
     }
 
     @Override
     public void onAnimationRepeat(Animator animation) {
 
     }
+
+    /**
+     * 获取当前进度
+     */
+    public int getProgress() {
+        return mProgress / 10;
+    }
+
+    public void setAnimProgressListener(AnimProgressListener animProgressListener) {
+        mAnimProgressListener = animProgressListener;
+    }
+
+    public interface AnimProgressListener {
+        /**
+         * 进度更新
+         */
+        void progressUpdate(int progress);
+    }
+
 }
--- DownloadView/app/src/main/java/com/xfhy/downloadview/MainActivity.java
+++ DownloadView/app/src/main/java/com/xfhy/downloadview/MainActivity.java
@@ -2,12 +2,55 @@ package com.xfhy.downloadview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, DownloadView.AnimProgressListener {
+
+    private DownloadView mDownloadView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+        mDownloadView = findViewById(R.id.view_download);
+        mDownloadView.setDuration(9900);
+        mDownloadView.setAnimProgressListener(this);
+
+        findViewById(R.id.btn_start).setOnClickListener(this);
+        findViewById(R.id.btn_pause).setOnClickListener(this);
+        findViewById(R.id.btn_continue).setOnClickListener(this);
+        findViewById(R.id.btn_stop).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_start:
+                mDownloadView.start();
+                break;
+            case R.id.btn_pause:
+                mDownloadView.pause();
+                break;
+            case R.id.btn_continue:
+                mDownloadView.continueAnim();
+                break;
+            case R.id.btn_stop:
+                mDownloadView.stop();
+                break;
+
+        }
+    }
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    public void progressUpdate(int progress) {
+        Log.w(TAG, "progressUpdate: progress=" + progress);
     }
 }
