f1981ea4cd5939fffc97c908bb9ccfb9fc1cbede
xfhy666 <xfhy666@gmail.com>
2019-05-05 14:52:48 +0800

    Scroller 模仿ViewPager

---
+++ ScrollerTest/app/src/main/java/com/xfhy/scrollertest/MainActivity.java
@@ -0,0 +1,16 @@
+package com.xfhy.scrollertest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ TitleView/app/src/main/java/com/xfhy/titleview/MainActivity.java
@@ -0,0 +1,21 @@
+package com.xfhy.titleview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        View titleView = findViewById(R.id.title_view);
+        Log.w(TAG, "onCreate: ");
+    }
+}
