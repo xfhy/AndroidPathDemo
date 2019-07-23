0bb31d7251abe7820ac906c49c5fa2da496324f3
xfhy <xfhy666@gmail.com>
2019-07-23 23:30:25 +0800

    复习retrofit

---
+++ RetrofitDemo/app/src/main/java/com/xfhy/retrofitdemo/Article.kt
@@ -0,0 +1,8 @@
+package com.xfhy.retrofitdemo
+
+/**
+ * Created by xfhy on 2019/7/23 22:39
+ * Description :
+ */
+class Article {
+}
---
+++ RetrofitDemo/app/src/main/java/com/xfhy/retrofitdemo/IArticleApi.kt
@@ -0,0 +1,8 @@
+package com.xfhy.retrofitdemo
+
+/**
+ * Created by xfhy on 2019/7/23 23:02
+ * Description :
+ */
+interface IArticleApi {
+}
